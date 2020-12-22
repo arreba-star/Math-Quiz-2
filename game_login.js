0be34e3b272f96d0be8d76ddc35bc169dad1be6c
arreba-star <75185381+arreba-star@users.noreply.github.com>
2020-12-21 18:00:40 -0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,14 @@
+player1_name = document.getElementById("player1_name_input").value;
+player2_name = document.getElementById("player2_name_input"). value;
+player1_score = 0;
+player2_score = 0;
+
+
+function addUser() {
+
+    
+    localStorage.setItem("player1_name" , player1_name);    
+    localStorage.setItem("player2_name" , player2_name);
+
+    window.location = "game_page.html";
+}
